b2b6cf8ab6102becdf1360dd461a0f0e4c7ed280
argonaultes <gael@argonaultes.fr>
2024-11-08 17:49:25 +0100

    ids sent in identifier

--- login_newsletter.py
+++ login_newsletter.py
@@ -6,8 +6,11 @@ class NewsletterUser(HttpUser):
     def login_to_newsletter(self):
         self.client.get("/welcome")
         time.sleep(5)
-        with self.client.get("/welcome?identifier=AQ", catch_response=True) as response:
-            if "AQ".lower() not in response.text.lower():
-                response.failure("User AQ not logged in")
+        with open('./ids.csv', 'r') as id_file:
+            for line in id_file.readlines():
+                identifier = line.strip()
+                with self.client.get(f"/welcome?identifier={identifier}", catch_response=True) as response:
+                    if identifier.lower() not in response.text.lower():
+                        response.failure(f"User {identifier} not logged in")
 
 
